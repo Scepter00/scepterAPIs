5bf914cfae1663d36aa635f500a0ea916945d279
scepter <chukwunenyeebuka@gmail.com>
2024-02-16 13:20:40 +0100

    update

--- src/main/java/com/application/scepter/data/repositories/TopicRepository.java
+++ src/main/java/com/application/scepter/data/repositories/TopicRepository.java
@@ -1,4 +1,7 @@
 package com.application.scepter.data.repositories;
 
-public interface TopicRepository {
+import com.application.scepter.data.models.Topic;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
 }
