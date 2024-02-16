b4fa1138d76498950d802fe480950deda717a1b5
scepter <chukwunenyeebuka@gmail.com>
2024-02-16 12:57:01 +0100

    update

--- src/main/java/com/application/scepter/data/models/Topic.java
+++ src/main/java/com/application/scepter/data/models/Topic.java
@@ -1,5 +1,7 @@
 package com.application.scepter.data.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +11,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @Setter
 @Getter
+@Entity
 public class Topic {
+
+    @Id
     private String id;
     private String name;
     private String description;
