6b7868de92ed48b32331f11671ac5cc4855ebd10
scepter <chukwunenyeebuka@gmail.com>
2024-02-16 16:34:08 +0100

    update

--- src/main/java/com/application/scepter/services/TopicService.java
+++ src/main/java/com/application/scepter/services/TopicService.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+import java.util.Optional;
 
 @Service
 public class TopicService {
@@ -24,26 +23,32 @@ public class TopicService {
         ));
 
     public List<Topic> getAllTopics() {
+        //return topics;
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
-    public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    public Optional<Topic> getTopic(String id) {
+        //return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findById(id);
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        //topics.add(topic);
+        topicRepository.save(topic);
     }
 
 
     public void updateTopic(String id, Topic topic) {
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        //for (int i = 0; i < topics.size(); i++) {
+            //Topic t = topics.get(i);
+            //if (t.getId().equals(id)) {
+                //topics.set(i, topic);
+                //return;
+            //}
+        //}
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
